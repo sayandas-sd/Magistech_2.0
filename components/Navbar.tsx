458fc6585b852602277d16164bbf0280a7b820e3
sayandas-sd <sayandas@Sayans-MacBook-Air.local>
2024-09-14 20:59:16 +0530

    added brochure in brochure link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -105,7 +105,7 @@ export default function Navbar() {
             </li>
             <li>
               <Link
-                href="#"
+                href="https://drive.google.com/file/d/1p47FNo9kkDTWdZ2zHknTIkj5bBOAanH6/view?usp=drivesdk"
                 className="block py-2 px-3 text-white rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Brochure
